0e13beb86899823363951f9eac2d4b02ecee2888
Tsani Rhodes <rhodestsani@gmail.com>
2022-05-04 00:21:46 -0400

    post working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import InputForm from './components/InputForm';
+// import Roster from './components/Roster';
 
 function App() {
   return (
     <div className="App">
      <Header/>
      <InputForm/>
+     {/* <Roster/> */}
     </div>
   );
 }
--- src/components/InputForm.jsx
+++ src/components/InputForm.jsx
@@ -1,45 +1,109 @@
 import React, {useState} from "react";
+import axios from "axios";
 
 const InputForm = () => {
-    const [name, setName] = useState("");
-    const [course, setCourse] = useState("");
-    const [gpa, setGpa] = useState("");
+    const [student, setStudent] = useState({
+        name: '',
+        course: '',
+        gpa: '',
+    })
+    
 
     const handleSubmit = (e) => {
         e.preventDefault();
     }
+    const addStudent = () => {
+        axios.post('http://localhost:3000/students',student)
+    }
     return(
         <div className="input-form">
             <form onSubmit={handleSubmit}>
+                <div><h3>Add Student</h3></div>
                  <div className="name">
                 <label> Name: 
                     <input type="text"
-                    placeholder="Enter name here..."
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}/>
+                    placeholder="Enter name here"
+                    value={student.name}
+                    onChange={(e) => {setStudent({ ...student, name: e.target.value})}}/>
                 </label>
                 </div>
                 <br></br> 
                 <div className="course">  
                 <label> Course: 
                     <input type="text"
-                    placeholder="Enter course here..."
-                    value={course}
-                    onChange={(e) => setCourse(e.target.value)}/>
+                    placeholder="Enter course here"
+                    value={student.course}
+                    onChange={(e) => {setStudent({ ...student, course: e.target.value})}}/>
                 </label>
                 </div>
                 <br></br> 
                 <div className="gpa"> 
                 <label>GPA: 
                     <input type="text"
-                    placeholder="Enter GPA here..."
-                    value={gpa}
-                    onChange={(e) => setGpa(e.target.value)}/>
+                    placeholder="Enter gpa here"
+                    value={student.gpa}
+                    onChange={(e) => {setStudent({ ...student, gpa: e.target.value})}}/>
                 </label>
                 </div>
-                {/* <button onClick={}>Submit</button> */}
+                <button onClick={addStudent}>Submit</button>
             </form>  
         </div>
     )
 }
 export default InputForm;
+
+
+// import React, {useState} from "react";
+// import axios from "axios";
+
+// const InputForm = () => {
+//     const [name, setName] = useState("");
+//     const [course, setCourse] = useState("");
+//     const [gpa, setGpa] = useState("");
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+//     }
+//     const addStudent = () => {
+//         axios.post('http://localhost:3000/students',student)
+//     }
+//     return(
+//         <div className="input-form">
+//             <form onSubmit={handleSubmit}>
+//                 <div><h3>Add Student</h3></div>
+//                  <div className="name">
+//                 <label> Name: 
+//                     <input type="text"
+//                     placeholder="Enter name here..."
+//                     value={name}
+//                     onChange={(e) => setName(e.target.value)}/>
+//                 </label>
+//                 </div>
+//                 <br></br> 
+//                 <div className="course">  
+//                 <label> Course: 
+//                     <input type="text"
+//                     placeholder="Enter course here..."
+//                     value={course}
+//                     onChange={(e) => setCourse(e.target.value)}/>
+//                 </label>
+//                 </div>
+//                 <br></br> 
+//                 <div className="gpa"> 
+//                 <label>GPA: 
+//                     <input type="text"
+//                     placeholder="Enter GPA here..."
+//                     value={gpa}
+//                     onChange={(e) => setGpa(e.target.value)}/>
+//                 </label>
+//                 </div>
+//                 <button onClick={addStudent}>Submit</button>
+//             </form>  
+//         </div>
+//     )
+// }
+// export default InputForm;
+
+
+
+
